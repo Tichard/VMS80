9786874bd950c1fe3ff2be0ed0c360b0432e5a68
Tichard <richard.taupiac@gmail.com>
2025-05-10 13:09:27 +0200

    [FIX] Biquad butterworth coefficients. + wrong coeff idx.

--- VMS80/Classes/Plugins/Biquad.cs
+++ VMS80/Classes/Plugins/Biquad.cs
@@ -3,40 +3,46 @@ namespace VMS80.Plugin
 {
     internal class Biquad
     {
-        private int m_order;
+        // Using series of Butterworth 2nd order filters because :
+        // * 2nd order is always stable
+        // * Butterworth even-order filers can be summed up with a perfeclty flat amplitude response
+
+        private int m_nb_biqud;
 
         private double[] coeffs = [];
         private double[] buffer = [];
 
-        public void init_hi_pass(float frequency, int order)
+        public void init_low_pass(float a_frequency, int a_nb_biqud)
         {
-            m_order = order;
-            int nb_coeffs = order * 5;
-            int nb_buffer = order * 2;
+            m_nb_biqud = a_nb_biqud;
+            int nb_coeffs = m_nb_biqud * 5;
+            int nb_buffer = m_nb_biqud * 2;
             int i, coeffs_idx;
             double a0, a1, a2, b0, b1, b2;
 
             coeffs = new double[nb_coeffs];
 
-            double ita = 1.0 / Math.Tan(Math.PI * frequency);
+            double ita = 1.0 / Math.Tan(Math.PI * a_frequency);
             double q = Math.Sqrt(2.0);
-            for (i = 0; i < m_order; ++i)
+            double g = 1.0 / (1.0 + q * ita + ita * ita);
+
+            for (i = 0; i < m_nb_biqud; ++i)
             {
                 coeffs_idx = i * 5;
 
-                b0 = 1.0 / (1.0 + q * ita + ita * ita);
-                b1 = -2.0 * b0;
-                b2 = b0;
+                b0 = g;
+                b1 = 2.0 * g;
+                b2 = g;
                 a0 = 1.0;
-                a1 = -2.0 * (ita * ita - 1.0) * b0;
-                a2 = -(1.0 - q * ita + ita * ita) * b0;
+                a1 = 2.0 * (ita * ita - 1.0) * g;
+                a2 = -(1.0 - q * ita + ita * ita) * g;
 
                 // Normalize by a0
                 coeffs[coeffs_idx + 0] = b0 / a0;
                 coeffs[coeffs_idx + 1] = b1 / a0;
                 coeffs[coeffs_idx + 2] = b2 / a0;
-                coeffs[coeffs_idx + 3] = -a1 / a0;
-                coeffs[coeffs_idx + 4] = -a2 / a0;
+                coeffs[coeffs_idx + 3] = a1 / a0;
+                coeffs[coeffs_idx + 4] = a2 / a0;
             }
 
             buffer = new double[nb_buffer];
@@ -46,35 +52,37 @@ namespace VMS80.Plugin
             }
         }
 
-        public void init_low_pass(float frequency, int order)
+        public void init_hi_pass(float a_frequency, int a_nb_biqud)
         {
-            m_order = order;
-            int nb_coeffs = order * 5;
-            int nb_buffer = order * 2;
+            m_nb_biqud = a_nb_biqud;
+            int nb_coeffs = m_nb_biqud * 5;
+            int nb_buffer = m_nb_biqud * 2;
             int i, coeffs_idx;
             double a0, a1, a2, b0, b1, b2;
 
             coeffs = new double[nb_coeffs];
 
-            double ita = 1.0 / Math.Tan(Math.PI * frequency);
+            double ita = 1.0 / Math.Tan(Math.PI * a_frequency);
             double q = Math.Sqrt(2.0);
-            for (i = 0; i < m_order; ++i)
+            double g = 1.0 / (1.0 + q * ita + ita * ita);
+
+            for (i = 0; i < m_nb_biqud; ++i)
             {
                 coeffs_idx = i * 5;
-                
-                b0 = 1.0 / (1.0 + q * ita + ita * ita);
-                b1 = 2.0 * b0;
-                b2 = b0;
+
+                b0 = (ita * ita) * g;
+                b1 = -2.0 * (ita * ita) * g;
+                b2 = (ita * ita) * g;
                 a0 = 1.0;
-                a1 = 2.0 * (ita * ita - 1.0) * b0;
-                a2 = -(1.0 - q * ita + ita * ita) * b0;
+                a1 = 2.0 * (ita * ita - 1.0) * g;
+                a2 = -(1.0 - q * ita + ita * ita) * g;
 
                 // Normalize by a0
                 coeffs[coeffs_idx + 0] = b0 / a0;
                 coeffs[coeffs_idx + 1] = b1 / a0;
                 coeffs[coeffs_idx + 2] = b2 / a0;
-                coeffs[coeffs_idx + 3] = -a1 / a0;
-                coeffs[coeffs_idx + 4] = -a2 / a0;
+                coeffs[coeffs_idx + 3] = a1 / a0;
+                coeffs[coeffs_idx + 4] = a2 / a0;
             }
 
             buffer = new double[nb_buffer];
@@ -89,15 +97,15 @@ namespace VMS80.Plugin
             double data_out = (double)a_data;
             double data_in;
             int coeffs_idx, buffer_idx;
-            for (int i = 0; i < m_order; ++i)
+            for (int i = 0; i < m_nb_biqud; ++i)
             {
                 coeffs_idx = i * 5;
                 buffer_idx = i * 2;
 
                 data_in = data_out; // serialize the biquads
                 data_out = data_in * coeffs[coeffs_idx + 0] + buffer[buffer_idx + 0];
-                buffer[buffer_idx + 0] = data_in * coeffs[coeffs_idx + 1] + data_out * coeffs[buffer_idx + 3] + buffer[buffer_idx + 1];
-                buffer[buffer_idx + 1] = data_in * coeffs[coeffs_idx + 2] + data_out * coeffs[buffer_idx + 4];
+                buffer[buffer_idx + 0] = data_in * coeffs[coeffs_idx + 1] + data_out * coeffs[coeffs_idx + 3] + buffer[buffer_idx + 1];
+                buffer[buffer_idx + 1] = data_in * coeffs[coeffs_idx + 2] + data_out * coeffs[coeffs_idx + 4];
             }
 
             return (float)data_out;
