59c85d6a218eb976cdcb0aa5fbde93cdf83e52d6
Tichard <richard.taupiac@gmail.com>
2025-05-11 17:03:28 +0200

    [CLEAN] Disable any RIAA processing + fix typo.

--- VMS80/Classes/Plugins/RiaaFilter.cs
+++ VMS80/Classes/Plugins/RiaaFilter.cs
@@ -8,12 +8,12 @@ namespace VMS80.Plugin
         private int m_samplerate;
 
         private readonly Biquad m_filter_L;
-        private readonly Biquad m_filer_R;
+        private readonly Biquad m_filter_R;
 
         public RIAAFilter()
         {
             m_filter_L = new Biquad();
-            m_filer_R = new Biquad();
+            m_filter_R = new Biquad();
         }
 
         public void process(float[] a_data, int a_nb_samples, int a_nb_channels)
@@ -23,15 +23,15 @@ namespace VMS80.Plugin
             {
                 for (int i = 0; i < a_nb_samples; ++i)
                 {
-                    a_data[2 * i + 0] = m_filter_L.process(a_data[2 * i + 0]);
-                    a_data[2 * i + 1] = m_filer_R.process(a_data[2 * i + 0]);
+                    //a_data[2 * i + 0] = m_filter_L.process(a_data[2 * i + 0]);
+                    //a_data[2 * i + 1] = m_filter_R.process(a_data[2 * i + 1]);
                 }
             }
             else
             {
                 for (int i = 0; i < a_nb_samples; ++i)
                 {
-                    a_data[i] = m_filter_L.process(a_data[i]);
+                    //a_data[i] = m_filter_L.process(a_data[i]);
                 }
             }
         }
@@ -41,7 +41,7 @@ namespace VMS80.Plugin
             m_samplerate = a_samplerate;
 
             //m_filter_L.init_riaa(m_samplerate, nb_biquad);
-            //m_filer_R.init_riaa(m_samplerate, nb_biquad);
+            //m_filter_R.init_riaa(m_samplerate, nb_biquad);
         }
 
     }
