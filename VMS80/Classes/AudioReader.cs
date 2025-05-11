703b18a6602eaffd272bba1f0b1af33762576057
Tichard <richard.taupiac@gmail.com>
2025-05-11 17:17:39 +0200

    [FIX] WAV header chunks can have variable size.

--- VMS80/Classes/AudioReader.cs
+++ VMS80/Classes/AudioReader.cs
@@ -17,12 +17,24 @@ namespace VMS80
                 BinaryReader reader = new(fs);
 
                 // chunk 0
-                int chunkID = reader.ReadInt32();
+                int chunkID = reader.ReadInt16();
+                while (chunkID != 18770) // Look for the first 16bits of chunkID
+                {
+                    chunkID = reader.ReadInt16();
+                }
+                chunkID = reader.ReadInt16(); // rest of chunkID (17990)
+
                 int fileSize = reader.ReadInt32();
                 int riffType = reader.ReadInt32();
 
                 // chunk 1
-                int fmtID = reader.ReadInt32();
+                int fmtID = reader.ReadInt16();
+                while (fmtID != 28006) // Look for the first 16bits of fmtID
+                {
+                    fmtID = reader.ReadInt16();
+                }
+                fmtID = reader.ReadInt16(); // rest of fmtID (8308)
+
                 int fmtSize = reader.ReadInt32(); // bytes for this chunk (expect 16 or 18)
 
                 // 16 bytes coming...
@@ -33,15 +45,14 @@ namespace VMS80
                 int fmtBlockAlign = reader.ReadInt16();
                 int bitDepth = reader.ReadInt16();
 
-                if (fmtSize == 18)
-                {
-                    // Read any extra values
-                    int fmtExtraSize = reader.ReadInt16();
-                    reader.ReadBytes(fmtExtraSize);
-                }
-
                 // chunk 2
-                int dataID = reader.ReadInt32();
+                int dataID = reader.ReadInt16();
+                while (dataID != 24932) // Look for the first 16bits of dataID
+                {
+                    dataID = reader.ReadInt16();
+                }
+                dataID = reader.ReadInt16(); // rest of dataID (24948)
+
                 int bytes = reader.ReadInt32();
 
                 // DATA
@@ -57,12 +68,12 @@ namespace VMS80
                 {
                     case 32:
                         Int32[] data32 = new Int32[nValues];
-                        Buffer.BlockCopy(byteArray, 0, data32, 0, bytes);
+                        System.Buffer.BlockCopy(byteArray, 0, data32, 0, bytes);
                         a_data = Array.ConvertAll(data32, e => e / (float)(Int32.MaxValue));
                         return true;
                     case 16:
                         Int16[] data16 = new Int16[nValues];
-                        Buffer.BlockCopy(byteArray, 0, data16, 0, bytes);
+                        System.Buffer.BlockCopy(byteArray, 0, data16, 0, bytes);
                         a_data = Array.ConvertAll(data16, e => e / (float)(Int16.MaxValue));
                         return true;
                     default:
